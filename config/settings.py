f1c9f43c729c7760c7b42470c174a4fb8d4172da
Ismat Dev <ismatismoilov709@gmail.com>
2026-02-21 17:34:34 +0500

    deploying

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ ALLOWED_HOSTS = [
     "127.0.0.1",
 ]
 CSRF_TRUSTED_ORIGINS = [
-    "http://cosmos-edu.duckdns.org",
+    "https://cosmos-edu.duckdns.org",
 ]
 
 AUTH_USER_MODEL = "users.UserProfile"
