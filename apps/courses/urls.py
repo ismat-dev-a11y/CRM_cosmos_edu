8be70d593ebf9c67fb0cf89a8229430c26a66ac1
Ismat Dev <ismatismoilov709@gmail.com>
2026-02-24 11:21:47 +0500

    fix: CourseCreateSerializer updated

--- apps/courses/urls.py
+++ apps/courses/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path("course/list", CourseListView.as_view()),
-    path("course/create", CourseCreateView.as_view()),
-    path("course/action/<int:pk>", CourseDetailAPIView.as_view()),
+    path("course/", CourseListView.as_view()),
+    path("course/", CourseCreateView.as_view()),
+    path("course/<int:pk>", CourseDetailAPIView.as_view()),
     path("lesson", LessonListView.as_view()),
-    path("lesson/create", LessonCreateView.as_view()),
+    path("lesson/", LessonCreateView.as_view()),
     path("lesson/<int:pk>", LessonRetrieveUpdateDestroyAPIView.as_view()),
 ]
