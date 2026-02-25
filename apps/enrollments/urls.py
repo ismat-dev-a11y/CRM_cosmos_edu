1180cc26287c4523c372412fefd3dda717a507a9
Ismat Dev <ismatismoilov709@gmail.com>
2026-02-25 13:20:47 +0500

    fix: serializers

--- apps/enrollments/urls.py
+++ apps/enrollments/urls.py
@@ -9,7 +9,7 @@ router.register("assignments", AssignmentViewSet, basename="assignment")
 
 urlpatterns = [
     path("enrollment", EnrollmentCreateAPIView.as_view()),
-    path("enrollment", EnrollmentListAPIView.as_view()),
+    path("enrollment-list", EnrollmentListAPIView.as_view()),
     path("enrollment/<int:pk>", EnrollmentUpdateDeleteAPIView.as_view()),
     path("attendence", AttendenceAPIView.as_view()),
     path("attendence-list", AttendenceListAPIView.as_view()),
